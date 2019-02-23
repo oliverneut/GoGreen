0b35894d72c8c6042cce8586abbe452761c0e314
Michael Chan <J.M.Chan@student.tudelft.nl>
2019-02-23 20:55:35 +0100

    We probably wont use the login obj.

---
+++ src/test/java/Client/LoginTest.java
@@ -0,0 +1,24 @@
+package Client;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginTest {
+
+    @Test
+    public void getUserName() {
+    }
+
+    @Test
+    public void getPassWord() {
+    }
+
+    @Test
+    public void toString() {
+    }
+
+    @Test
+    public void equals() {
+    }
+}
